46865c4cbb9b123e87c08a0eeffaa0644eafdc40
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-31 13:31:20 +0500

    web api study

---
+++ EmployeeProject/Controllers/OnlineController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OnlineController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetActionResult()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                var result = await client.GetAsync("https://jsonplaceholder.typicode.com/comments").Result.Content.ReadAsStringAsync();
+                return Ok(result);
+            }
+        }
+    }
+}
